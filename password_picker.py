3b312cc5de8b1b6612fb003a1dad25f89788dde0
ksandoval21 <ksandoval-lopez21@basecampcodingacademy.org>
2021-01-10 11:26:40 -0600

    added save option

--- password_picker.py
+++ password_picker.py
@@ -1,5 +1,9 @@
 import string 
 import random
+import json
+with open("passwords.json") as file:
+    reader = json.load(file)
+    passwords = list(reader)
 adj=['pretty', 'red', 'smart', 'big', 'small', 'brave', 'handsome']
 noun=['apple', 'table', 'bottle', 'purse', 'toaster', 'pillow']
 print("Get a random uncrackable password")
@@ -9,3 +13,12 @@ num=random.randint(0,100)
 char=random.choice(string.punctuation)
 password=ran_adj + ran_noun + str(num) + char 
 print("Your new password is" + password)
+response=input('Would you like to save this password?')
+if response== "yes".lower():
+    user=input("Enter a username: ")
+    pin=input("Enter a pin: ")
+    data={"User": user, "Pin": pin, "Password": password}
+    with open("passwords.json", "w", newline='') as file:
+        json.dump(data, file)
+elif response=="no".lower():
+    print("Goodbye!")
